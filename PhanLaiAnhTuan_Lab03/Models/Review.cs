22160589dd8b3d2188fc4901a2ca16e91de9373a
CucHuynh04 <thanhcuc444@gmail.com>
2025-06-17 21:38:40 +0700

    Cap nhat bai lam

---
+++ PhanLaiAnhTuan_Lab03/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿namespace PhanLaiAnhTuan_Lab03.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public string UserId { get; set; }              // ✅ ID người dùng
+        public ApplicationUser User { get; set; }       // ✅ Navigation tới người dùng
+
+        public string Comment { get; set; }
+        public int Rating { get; set; } // 1 - 5 sao
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
